6e6b891420ba204f9d03c2fa80113f4ed83b7337
Kostyantyn Matviyevskyy <kostyamat@yahoo.com>
2025-12-11 18:23:17 +0100

    Працює ідеально!

--- app/src/main/java/com/qf/musicplayer/ui/MediaService.kt
+++ app/src/main/java/com/qf/musicplayer/ui/MediaService.kt
@@ -8,9 +8,6 @@ import android.util.Log
 class MediaService : Service() {
     override fun onBind(intent: Intent?): IBinder? {
         Log.d("MusicProxyService", "System tried to bind MediaService. Returning null.")
-        // Повертаємо null, оскільки у нас немає реального AIDL інтерфейсу.
-        // Це краще, ніж краш або відсутність сервісу.
-        // Якщо система суворо вимагає AIDL, тут треба буде реалізувати заглушку com.carsyso.mediasdk
         return null
     }
 
